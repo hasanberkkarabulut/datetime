4c574aee3b3a1fd9245f65d6290977ab31cd3cf9
hasanberkkarabulut <hasanberkarabulut@gmail.com>
2024-09-29 20:16:04 +0300

    Add files via upload

---
+++ Datetime/Datetime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DateTime Tarih = DateTime.Now;
+        }
+    }
+}
